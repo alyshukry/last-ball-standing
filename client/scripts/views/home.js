bafeac3dda567ded0262eb248006984f76679fe8
Aly Shukry <alyshukry.dev@gmail.com>
2026-07-05 00:04:01 +0300

    ui changes

--- client/scripts/views/home.js
+++ client/scripts/views/home.js
@@ -140,4 +140,4 @@ const createBannerBalls = (container, count, scale = 1) => {
     }
 }
 
-createBannerBalls(document.querySelector('#banner #balls-container'), 10, .85)
+createBannerBalls(document.querySelector('#banner #balls-container'), 10, 1)
